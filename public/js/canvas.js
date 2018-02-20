17be094252800106a8bc937e73329585193909f3
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-02-20 18:06:25 +0700

    Add modal-body, modal when click change

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -11,8 +11,22 @@ let nodeBlur = new fabric.Circle({
   fill: 'blue',
   hoverCursor: 'pointer',
   type: 'node',
-  name: 'Blur',
+  name: 'GaussianBlur',
   id: '5555555555555',
+  limitInput: 1,
+
+  settings: {
+    'sigmaX': {
+      type: 'input',
+      value: 20,
+      default_value: 20
+    },
+    'sigmaY': {
+      type: 'input',
+      value: 0,
+      default_value: 0
+    }
+  }
 })
 let nodeRotate = new fabric.Circle({
   radius: 50,
@@ -23,6 +37,17 @@ let nodeRotate = new fabric.Circle({
   type: 'node',
   name: 'Rotate',
   id: '9999999999999',
+  limitInput: 1,
+  
+  settings: {
+    'angle': {
+      type: 'slider',
+      value: 0,
+      default_value: 0,
+      min_value: 0,
+      max_value: 360
+    }
+  }
 })
 canvas.add(nodeBlur, nodeRotate)
 
@@ -36,11 +61,75 @@ $(() => {
     className: 'data-title',
     callback: (key, options) => {
       if (key === 'delete') {
+        // event: delete node
         if (confirm("Are you want to delete this?"))
           canvas.remove(objectTarget)
+      } else if (key === 'settings') {
+        // event: node setting
+        $('#modal-title').text(`type: ${objectTarget.type}, id: ${objectTarget.id}`)
+        $('#modal-content').html('') // clear html
+
+        // Add html to modal-body
+        for (setting in objectTarget.settings) {
+          let objectId = `${setting}_${objectTarget.id}`,
+              settings = objectTarget.settings,
+              html = ''
+          html += `<div class="form-group">`
+          html += `<label for=${setting} class="form-control-label">${setting}:</label>`
+          if (settings[setting].type === 'input')
+            html += `<input type="text" class="form-control" id=${objectId}>`
+          else if (settings[setting].type === 'slider') {
+            html += `<div id=${objectId}>
+                      <div id=custom-${objectId} class="ui-slider-handle"></div>
+                    </div>`
+          }
+          html += `</div>`
+          $('#modal-content').append(html)
+
+          // Add event and value
+          if (settings[setting].type === 'slider') {
+            let handle = $(`#custom-${objectId}`)
+            $(`#${objectId}`).slider({
+              value: settings[setting].value,
+              min: settings[setting].min_value,
+              max: settings[setting].max_value,
+              create: () => {
+                handle
+                  .text(settings[setting].value)
+                  .css({
+                  'width': '2em',
+                  'height': '1.6em',
+                  'top': '50%',
+                  'margin-top': '-.8em',
+                  'text-align': 'center',
+                  'line-height': '1.6em',
+                })
+              },
+              slide: (event, ui) => {
+                handle.text(ui.value)
+              }
+            })
+          } else if (settings[setting].type === 'input')
+            $(`#${objectId}`).val(settings[setting].value)
+        }
+
+        // modal onclick save
+        $('#modal-save-settings').click(() => {
+          for (setting in objectTarget.settings) {
+            let objectId = `${setting}_${objectTarget.id}`,
+                settings = objectTarget.settings
+            if (settings[setting].type === 'slider')
+              settings[setting].value = $(`#${objectId}`).slider("value")
+            else
+              settings[setting].value = $(`#${objectId}`).val()
+            
+            $('#modal-settings').modal('hide')
+          }
+        })
+        $('#modal-settings').modal('toggle')
       }
 
-      objectTarget = null
+      // objectTarget = null
     },
     items: {
       "delete": {
@@ -54,6 +143,7 @@ $(() => {
       }
     }
   })
+
   $(selector).contextMenu(false)
 })
 
@@ -62,7 +152,7 @@ canvas.on('mouse:over', (options) => {
     objectTarget = options.target
 
     if (objectTarget.type === 'node') {
-      $('.data-title').attr('data-menutitle', objectTarget.name + ', ' + objectTarget.id)
+      $('.data-title').attr('data-menutitle', `${objectTarget.name}, ${objectTarget.id}`)
       $('canvas').contextMenu(true)
     }
   }
