73bb453e6c897f849c1e7fd96922f824c48bc40c
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-02-21 13:57:22 +0700

    Move to src dir

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'popper.js'
+import 'bootstrap'
+import 'jquery-ui-bundle'
+import 'jquery-contextmenu'
+
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/font-awesome/css/font-awesome.css'
+import '../node_modules/jquery-contextmenu/dist/jquery.contextMenu.css'
+
+import './canvas'
