c454280a9e6b63002665ece1faec11b8480df6cd
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-02-22 17:02:44 +0700

    Add index is backup for dev

---
+++ src/drawing/index.js
@@ -0,0 +1,25 @@
+import Canvas from './canvas'
+import Setting from './setting'
+import Drawing from './drawing'
+
+const canvas = new Canvas('drawing-canvas', 1500, 1500)
+const setting = new Setting()
+
+const nodeBlur = canvas.createNode('GaussianBlur', undefined, 50, 50, {
+  'sigmaX': setting.addSetting('input', 20, 20),
+  'sigmaY': setting.addSetting('input', 0, 0)
+})
+
+const nodeRotate = canvas.createNode('Rotate', undefined, 170, 170, {
+  'angle': setting.addSetting('slider', 0, 0, 0, 360)
+})
+
+const nodeZoom = canvas.createNode('Zoom', undefined, 250, 50, {
+  'scale': setting.addSetting('slider', 0, 0, -50, 50)
+})
+
+canvas.addObject(nodeBlur, nodeRotate, nodeZoom)
+canvas.renderAll()
+
+const drawing = new Drawing(canvas)
+drawing.start()
