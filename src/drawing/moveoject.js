7fc13d72f6ac6809508ab4c088dc319c49dcac09
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-02-23 00:36:13 +0700

    Add line update when object move

---
+++ src/drawing/moveoject.js
@@ -0,0 +1,23 @@
+export default class MoveObject {
+  constructor(canvas) {
+    this.canvas = canvas
+  }
+
+  start() {
+    this.canvas.onEventListener('object:moving', this.objectMoving.bind(this))
+  }
+
+  stop() {
+    this.canvas.offEventListener('object:moving')
+  }
+
+  objectMoving (opt) {
+    _.forEach(opt.target.lines, (lineElm) => {
+      if (opt.target.id === lineElm.beginId)
+        lineElm.set({x1: opt.target.left, y1: opt.target.top})
+      else if (opt.target.id === lineElm.endId)
+        lineElm.set({x2: opt.target.left, y2: opt.target.top})
+      this.canvas.renderAll()
+    })
+  }
+}
