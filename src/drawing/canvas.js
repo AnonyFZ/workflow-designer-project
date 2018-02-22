8550bc3ec44bf73094446e2fad0bbe7f77a58aa4
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-02-22 17:02:11 +0700

    Add on/off eventListener

--- src/drawing/canvas.js
+++ src/drawing/canvas.js
@@ -12,7 +12,9 @@ export default class Canvas {
       perPixelTargetFind: true
     })
 
+    fabric.device
     fabric.Object.prototype.originX = fabric.Object.prototype.originY = 'center'
+    fabric.Object.prototype.hoverCursor = 'pointer'
     fabric.Object.prototype.hasControls = false
     fabric.Arrow = Arrow
 
@@ -43,16 +45,27 @@ export default class Canvas {
       name: name,
       left: left,
       top: top,
-      hoverCursor: 'pointer',
       limitInput: 1,
       lines: [],
       settings: settings
     })
+    .on('moving', (options) => {
+      let target = options.target
+    })
   }
 
-  addObject(object) {
+  addObject(...object) {
     if (_.isNil(object)) return
-    this.canvas.add(object)
+    _.forEach(object, (val) => {
+      this.canvas.add(val)
+    })
+  }
+
+  removeObject(...object) {
+    if (_.isNil(object)) return
+    _.forEach(object, (val) => {
+      this.canvas.remove(val)
+    })
   }
 
   generateId() {
@@ -65,12 +78,14 @@ export default class Canvas {
     return id
   }
 
-  createLine(points = [0, 0, 0, 0]) {
+  createLine(points = [0, 0, 0, 0], beginId = -1, endId = -1) {
     return new fabric.Arrow(points, {
       level: 0,
       type: 'arrow_line',
       strokeWidth: 2,
-      stroke: '#000'
+      stroke: '#000',
+      beginId: beginId,
+      endId: endId
     })
   }
 
@@ -79,8 +94,32 @@ export default class Canvas {
     _.sort((obj1, obj2) => {
       if (obj1.level === obj2.level)
         return obj1.width * obj1.height < obj2.width * obj2.height
-      return obj1.level < obj2.level
+      return obj1.level < obj2.level // debug arrow
     })
     this.canvas.renderAll()
+  }
+
+  onEventListener(event = {}, handle) {
+    this.eventListener(event, handle)
+  }
+
+  offEventListener(event) {
+    this.eventListener(event)
+  }
+
+  eventListener(event, handle) {
+    if (_.isNil(handle))
+      this.canvas.off(event)
+    else
+      this.canvas.on(event, handle)
+  }
+
+  getPointer(event) {
+    if (_.isNil(event)) return
+    return this.canvas.getPointer(event)
+  }
+
+  getCanvas() {
+    return this.canvas
   }
 }
