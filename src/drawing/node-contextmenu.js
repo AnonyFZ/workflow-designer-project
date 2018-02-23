83026c9969a658e780af4911db3e6d5250aa7f15
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-02-23 17:47:40 +0700

    Add settingsKeyContextmenu in node-contextmenu.js

--- src/drawing/node-contextmenu.js
+++ src/drawing/node-contextmenu.js
@@ -3,6 +3,9 @@ export default class NodeContextmenu {
     this.canvas = canvas
     this.canvas_id = `#${canvas.id}`
     this.canvas_class = `.${canvas.id}`
+    this.modal_settings = $('#modal-settings')
+    this.modal_title = $('#modal-title')
+    this.modal_body = $('#modal-body')
     this.target = null
   }
 
@@ -24,6 +27,7 @@ export default class NodeContextmenu {
       }
     })
 
+    $(this.canvas_class).contextMenu(false)
   }
 
   start() {
@@ -44,25 +48,104 @@ export default class NodeContextmenu {
     
     this.target = opt.target
     $('.data-title').attr('data-menutitle', `${opt.target.name}, ${opt.target.id}`)
-    $(this.canvas_id).contextMenu(true)
+
+    $(this.canvas_class).contextMenu(true)
   }
 
   mouseOut(opt) {
     if (_.isNil(opt.target)) return
     if (opt.target.type !== 'node') return
 
-    $(this.canvas_id).contextMenu(false)
+    $(this.canvas_class).contextMenu(false)
   }
 
   contextMenuCallback(key, opt) {
-    if (key === 'delete') {
-      console.log(this.canvas.hashTable)      
-      _.forEach(this.target.lines, (val) => {
-        this.canvas.removeObject(val)
+    if (key === 'delete')
+      this.deleteKeyContextMenu()
+    else if (key === 'settings')
+      this.settingsKeyContextMenu()
+  }
+
+  deleteKeyContextMenu() {
+    _.forEach(this.target.lines, (val) => {
+      this.canvas.removeObject(val)
+    })
+    this.canvas.removeObject(this.target)
+    this.target = null
+  }
+
+  settingsKeyContextMenu() {
+    this.modal_title.text(`${this.target.name}-${this.target.type}-${this.target.id}`)
+    this.modal_body.html('')
+
+    // create dynamic modal content
+    _.forEach(this.target.settings, this.createSettingElement.bind(this))
+
+    // event when mouse click save change
+    $('#modal-save-settings').click(() => {
+      _.forEach(this.target.settings, (val, key) => {
+        const object = $(`#${val.type}-${key}-${this.target.id}`)
+        if (val.type === 'slider')
+          val.value = object.slider('value')
+        else
+          val.value = object.val()
       })
-      this.canvas.removeObject(this.target)
-      this.target = null
-      console.log(this.canvas.hashTable)
+
+      $(this.modal_settings).modal('hide')
+    })
+
+    this.modal_settings.modal('show')
+  }
+
+  createSettingElement(val, key) {
+    const objectId = `${key}-${this.target.id}`
+    
+    // create common element
+    const form_group = $('<div>', {
+      class: 'form-group'
+    }).appendTo(this.modal_body)
+
+    const label = $('<label>', {
+      class: 'form-control-label',
+      for: objectId
+    }).text(key).appendTo(form_group)
+
+    if (val.type === 'input') {
+      // create input element
+      const input_form_control = $('<input>', {
+        class: 'form-control',
+        id: `${val.type}-${objectId}`,
+        type: 'text',
+      }).val(val.value).appendTo(form_group)
+
+    } else if (val.type === 'slider') {
+      // create slider element
+      const div_form_control = $('<div>', {
+        id: `${val.type}-${objectId}`
+      }).appendTo(form_group)
+
+      const div_ui_slider_handle = $('<div>', {
+        id: `custom-${objectId}`,
+        class: 'ui-slider-handle',
+        css: {
+          width: '2em',
+          height: '1.6em',
+          top: '50%',
+          'margin-top': '-.8em',
+          'text-align': 'center',
+          'line-height': '1.6em'
+        }
+      }).text(val.value).appendTo(div_form_control)
+
+      // add slider event
+      div_form_control.slider({
+          value: val.value,
+          min: val.min_value,
+          max: val.max_value,
+          slide: (e, ui) => {
+            div_ui_slider_handle.text(ui.value)
+          }
+      })
     }
   }
 }
