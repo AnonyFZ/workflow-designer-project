68f02ee526f186f28d7944b7285916559977dae2
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-02-23 17:48:35 +0700

    Edit createNode pass object settings in index.js

--- src/drawing/index.js
+++ src/drawing/index.js
@@ -8,17 +8,33 @@ import NodeContextmenu from './node-contextmenu'
 const canvas = new Canvas('drawing-canvas', 1500, 1500)
 const setting = new Setting()
 
-const nodeBlur = canvas.createNode('GaussianBlur', undefined, 50, 50, {
-  'sigmaX': setting.addSetting('input', 20, 20),
-  'sigmaY': setting.addSetting('input', 0, 0)
+const nodeBlur = canvas.createNode('GaussianBlur', undefined, 50, 50, 1, {
+  'sigmaX': setting.addSetting('input', {
+    value: 20,
+    default_value: 20,
+  }),
+  'sigmaY': setting.addSetting('input', {
+    value: 0,
+    default_value: 0,
+  })
 })
 
-const nodeRotate = canvas.createNode('Rotate', undefined, 170, 170, {
-  'angle': setting.addSetting('slider', 0, 0, 0, 360)
+const nodeRotate = canvas.createNode('Rotate', undefined, 170, 170, 1, {
+  'angle': setting.addSetting('slider', {
+    value: 0,
+    default_value: 0,
+    min_value: 0,
+    max_value: 360
+  })
 })
 
-const nodeZoom = canvas.createNode('Zoom', undefined, 250, 50, {
-  'scale': setting.addSetting('slider', 0, 0, -50, 50)
+const nodeZoom = canvas.createNode('Zoom', undefined, 250, 50, 1, {
+  'scale': setting.addSetting('slider', {
+    value: 0,
+    default_value: 0,
+    min_value: -50,
+    max_value: 50
+  })
 })
 
 canvas.addObject(nodeBlur, nodeRotate, nodeZoom)
