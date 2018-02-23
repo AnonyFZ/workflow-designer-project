4fea8b10c5c69778c6680a6ec6630c782174ebc5
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-02-24 04:35:11 +0700

    Add css/app.css to webpack

--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ import '../node_modules/jquery-ui-bundle/jquery-ui.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/font-awesome/css/font-awesome.css'
 import '../node_modules/jquery-contextmenu/dist/jquery.contextMenu.css'
+import '../public/css/app.css'
 
 import './drawing'
