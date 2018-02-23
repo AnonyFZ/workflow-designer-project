180ed0cc2e34a3bab701379f1baab14608a17e7e
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-02-23 17:23:35 +0700

    Add jquery-ui css to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import 'bootstrap'
 import 'jquery-ui-bundle'
 import 'jquery-contextmenu'
 
+import '../node_modules/jquery-ui-bundle/jquery-ui.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/font-awesome/css/font-awesome.css'
 import '../node_modules/jquery-contextmenu/dist/jquery.contextMenu.css'
